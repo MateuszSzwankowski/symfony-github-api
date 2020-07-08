059cd138be769ea9d01ff5170019150dec055f89
Mateusz Szwankowski <mateusz.szwankowski@globegroup.pl>
2020-07-08 23:14:22 +0200

    fix message display for GithubAuthorizationException

--- src/Exception/GithubAuthorizationException.php
+++ src/Exception/GithubAuthorizationException.php
@@ -6,5 +6,9 @@ use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 
 class GithubAuthorizationException extends UnauthorizedHttpException
 {
-    
+    public function __construct(string $message = null, \Throwable $previous = null, ?int $code = 0, array $headers = [])
+    {
+        $challenge = 'token realm=\"api.github.com\"';
+        parent::__construct($challenge, $message, $previous, $code, $headers);
+    }
 }
