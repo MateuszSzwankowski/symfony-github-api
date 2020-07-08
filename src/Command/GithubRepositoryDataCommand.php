a79c73ce331c0dc7b610ed9796410fbfac99907c
Mateusz Szwankowski <mateusz.szwankowski@globegroup.pl>
2020-07-09 00:39:26 +0200

    add GithubRepositoryDataCommand

--- src/Command/GithubRepositoryDataCommand.php
+++ src/Command/GithubRepositoryDataCommand.php
@@ -8,40 +8,38 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
-use Symfony\Component\Serializer\SerializerInterface;
 
-class GithubUserDataCommand extends Command
+class GithubRepositoryDataCommand extends Command
 {
-    protected static $defaultName = 'app:github:user';
+    protected static $defaultName = 'app:github:repo';
 
     private GithubApiService $githubService;
-    private SerializerInterface $serializer;
     private ParameterBagInterface $params;
 
     public function __construct(
         GithubApiService $githubService,
-        SerializerInterface $serializer,
         ParameterBagInterface $params
     ) {
         parent::__construct();
         $this->githubService = $githubService;
-        $this->serializer = $serializer;
         $this->params = $params;
     }
     
     protected function configure()
     {
         $this
-            ->setDescription('Fetch user data from GitHub api.')
-            ->addArgument('login', InputArgument::REQUIRED, 'Searched Github login.')
+            ->setDescription('Fetch repository data from GitHub api.')
+            ->addArgument('login', InputArgument::REQUIRED, 'Github login of repository owner.')
+            ->addArgument('repoName', InputArgument::REQUIRED, 'Name of github repository.')
             ->addArgument('token', InputArgument::OPTIONAL, 'Github api token (optional).');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $responseDto = $this->githubService->getUserInfo(
+        $responseDto = $this->githubService->getRepositoryInfo(
             $input->getArgument('login'),
-            $input->getArgument('token') ?? $this->params->get('app.github_token')
+            $input->getArgument('repoName'),
+            $input->getArgument('token') ?? $this->params->get('app.github_token'),
         );
 
         $data = $this->serializer->serialize(
