259fa20434a0979f13f634d7bfa3c58781c54bfe
Mateusz Szwankowski <mateusz.szwankowski@globegroup.pl>
2020-07-08 21:42:02 +0200

    add github api exceptions

---
+++ src/Exception/GithubAuthorizationException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Exception;
+
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+
+class GithubAuthorizationException extends UnauthorizedHttpException
+{
+    
+}
---
+++ src/Exception/GithubConnectionException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Exception;
+
+use Symfony\Component\HttpKernel\Exception\ServiceUnavailableHttpException;
+
+class GithubConnectionException extends ServiceUnavailableHttpException
+{
+    
+}
